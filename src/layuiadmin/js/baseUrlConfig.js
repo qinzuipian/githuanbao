33dc968c35ff19d107c66b27ba3e88694ed3fdf6
xw <xw@qq.com>
2019-09-24 11:29:27 +0800

    设备，异常，机构等bug提交

--- src/layuiadmin/js/baseUrlConfig.js
+++ src/layuiadmin/js/baseUrlConfig.js
@@ -1,2 +1,2 @@
-var BASEURL = "http://192.168.1.188:9091/";
-// var BASEURL = "http://192.168.1.166:9091/";
+// var BASEURL = "http://192.168.1.188:9091/";
+var BASEURL = "http://192.168.1.166:9091/";
