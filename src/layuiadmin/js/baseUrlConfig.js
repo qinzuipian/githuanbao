e9802ad13849d7b3488e5ceca1a8c0d6b7bcfc59
xw <xw@qq.com>
2019-10-18 12:14:23 +0800

    能耗分析

--- src/layuiadmin/js/baseUrlConfig.js
+++ src/layuiadmin/js/baseUrlConfig.js
@@ -1,3 +1,4 @@
 // var BASEURL = "http://192.168.1.188:9091/";
-var BASEURL = "http://192.168.1.166:9091/";
+// var BASEURL = "http://192.168.1.166:9091/";
+var BASEURL = "http://192.168.1.14:9091/";
 // var BASEURL = "http://132.232.11.63:9091/";
