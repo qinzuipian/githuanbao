2e8c1d95f210f9ad048f3832dddd97b3275245e3
梁彪彪 <pokerface0206@163.com>
2019-12-09 12:47:29 +0800

    修改权限问题

--- src/layuiadmin/config.js
+++ src/layuiadmin/config.js
@@ -4,11 +4,7 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function (exports) {
     exports('setter', {
         container: 'LAY_app' //容器ID
         , base: layui.cache.base //记录静态资源所在路径
-        // , BASEURL: "http://192.168.1.188:9091/" //todo 接口路径
-        // , BASEURL: "http://132.232.11.63:9091/" //todo 接口路径
         // , BASEURL: "http://192.168.1.166:9091/" //todo 接口路径
-        // , BASEURL: "http://192.168.1.13:9091/" //todo 接口路径
-        // , BASEURL: "http://106.54.92.42:9091/" //todo 接口路径
         , BASEURL: "http://47.105.183.106:9091/" //todo 接口路径
         , views: layui.cache.base + 'tpl/' //动态模板所在目录
         , entry: 'index' //默认视图文件名
