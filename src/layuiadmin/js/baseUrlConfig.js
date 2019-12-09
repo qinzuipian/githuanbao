b91db8d1c98a4456c5a6699d367e8761a7bd4df7
xw <xw@qq.com>
2019-12-09 18:19:35 +0800

    地址修改

--- src/layuiadmin/js/baseUrlConfig.js
+++ src/layuiadmin/js/baseUrlConfig.js
@@ -3,4 +3,4 @@
 // var BASEURL = "http://192.168.1.13:9091/";
 // var BASEURL = "http://106.54.92.42:9091/";
 // var BASEURL = "http://132.232.11.63:9091/"; //todo 接口路径
-var BASEURL = "http://47.105.183.106:9091/"; //todo 接口路径
+var BASEURL = "http://47.105.183.106:9092/"; //todo 接口路径
