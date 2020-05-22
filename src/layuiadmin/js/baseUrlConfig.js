0ae5f987f0fd3dba503147abe5cf80685e5e45a8
xw <xw@qq.com>
2020-05-22 13:22:11 +0800

    修改提交

--- src/layuiadmin/js/baseUrlConfig.js
+++ src/layuiadmin/js/baseUrlConfig.js
@@ -1,7 +1,7 @@
 
-// var BASEURL = "http://192.168.1.166:9091/";
+var BASEURL = "http://192.168.1.166:9091/";
 // var BASEURL = "http://192.168.1.13:9091/";
 // var BASEURL = "http://106.54.92.42:9091/";
 // var BASEURL = "http://132.232.11.63:9091/"; //todo 接口路径
-var BASEURL = "http://47.105.183.106:9091/"; //todo 接口路径
+// var BASEURL = "http://47.105.183.106:9091/"; //todo 接口路径
 
